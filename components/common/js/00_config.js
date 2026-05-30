021a355db3c785d9a27ddd3e1bfd4394b0e41862
Victor Domtigo <rectors12@gmail.com>
2026-05-30 14:44:04 +0300

    123

--- components/common/js/00_config.js
+++ components/common/js/00_config.js
@@ -2,7 +2,7 @@
 // 00_config.js – Глобальные настройки сайта
 // ============================================================
 window.APP_CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzzkAdjoPZY_dqTysSGLxjjQH8SlbV6_WsXSfk2OTN3qZLRyi3Pr2J_bjUX3L3CbzbKZQ/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyoV7zITJkZYL6wsdL7LdrSNxz9392V5BQ2QlxupJNYAINzgx0wq32jyqJ_RJj6UNQwzA/exec',
     TELEGRAM: 'https://t.me/HrLubacheva',
     WHATSAPP: 'https://wa.me/79217916655',
     EMAIL: 'hrlubacheva@yandex.ru',
